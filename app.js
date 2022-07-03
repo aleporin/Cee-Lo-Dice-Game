403ce635019443eabb4da1e7db46ac99ab6b83fe
Austin LePorin <austinleporin@gmail.com>
2022-07-03 13:51:58 -0400

    First JS BTN yielding random numbers for computer

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+const playBtn = document.querySelector('#btn')
 
+const rollDiceBtn = document.querySelector('.rollDice')
 
+let d1 = document.querySelector('#d1')
+let d2 = document.querySelector('#d2')
+let d3 = document.querySelector('#d3')
+let d4 = document.querySelector('.d4')
+let d5 = document.querySelector('.d5')
+let d6 = document.querySelector('.d6')
 
+const computerRoll = () => {
+  d1.innerHTML = Math.floor(Math.random() * 6 + 1)
+  d2.innerHTML = Math.floor(Math.random() * 6 + 1)
+  d3.innerHTML = Math.floor(Math.random() * 6 + 1)
+}
+
+playBtn.addEventListener('click', computerRoll)
