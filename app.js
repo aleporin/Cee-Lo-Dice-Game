d6cd58f228d6cfc321e4772ffa65bbcd9cfa4fb1
Austin LePorin <austinleporin@gmail.com>
2022-07-04 11:47:04 -0400

    Yielding random nums for player and computer

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const playBtn = document.querySelector('#btn')
 
-const rollDiceBtn = document.querySelector('.rollDice')
+const rollDiceBtn = document.querySelector('#rollDice')
 
 let d1 = document.querySelector('#d1')
 let d2 = document.querySelector('#d2')
 let d3 = document.querySelector('#d3')
-let d4 = document.querySelector('.d4')
-let d5 = document.querySelector('.d5')
-let d6 = document.querySelector('.d6')
+let d4 = document.querySelector('#d4')
+let d5 = document.querySelector('#d5')
+let d6 = document.querySelector('#d6')
 
 const computerRoll = () => {
-  d1.innerHTML = Math.floor(Math.random() * 6 + 1)
-  d2.innerHTML = Math.floor(Math.random() * 6 + 1)
-  d3.innerHTML = Math.floor(Math.random() * 6 + 1)
+  const diceOne = (d1.innerHTML = Math.floor(Math.random() * 6 + 1))
+  const diceTwo = (d2.innerHTML = Math.floor(Math.random() * 6 + 1))
+  const diceThree = (d3.innerHTML = Math.floor(Math.random() * 6 + 1))
+}
+const playerRoll = () => {
+  d4.innerHTML = Math.floor(Math.random() * 6 + 1)
+  d5.innerHTML = Math.floor(Math.random() * 6 + 1)
+  d6.innerHTML = Math.floor(Math.random() * 6 + 1)
 }
 
 playBtn.addEventListener('click', computerRoll)
+
+rollDiceBtn.addEventListener('click', playerRoll)
