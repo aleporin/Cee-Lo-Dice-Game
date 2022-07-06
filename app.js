b954dc34fb5ad0b7d362db2781f1eeb5cd79c2ff
Austin LePorin <austinleporin@gmail.com>
2022-07-06 09:01:50 -0400

    Player conditions complete

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const rollDiceBtn = document.querySelector('#rollDice')
 
 let compStatus = document.querySelector('.compScore')
 
-let dice = []
+let playerStatus = document.querySelector('.playerScore')
+
+let compDice = []
+let playerDice = []
 
 let d1 = document.querySelector('#d1')
 let d2 = document.querySelector('#d2')
@@ -14,31 +17,62 @@ let d4 = document.querySelector('#d4')
 let d5 = document.querySelector('#d5')
 let d6 = document.querySelector('#d6')
 
+let gameOver = false
+
+let heiarchy = [
+  '1-2-3',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '1-1-1',
+  '2-2-2',
+  '3-3-3',
+  '4-4-4',
+  '5-5-5',
+  '6-6-6',
+  '4-5-6'
+]
+
 const computerRoll = () => {
   d1.innerHTML = Math.floor(Math.random() * 6 + 1)
   // d1 and add class name for respective dice background image d1.className = BG + d1.innerHTML
   d2.innerHTML = Math.floor(Math.random() * 6 + 1)
   d3.innerHTML = Math.floor(Math.random() * 6 + 1)
-  dice = []
-  dice.push(d1.innerHTML, d2.innerHTML, d3.innerHTML)
-  dice = dice.sort()
-  computerScore()
+  compDice = []
+  compDice.push(d1.innerHTML, d2.innerHTML, d3.innerHTML)
+  compDice = compDice.sort()
+  setTimeout(computerScore, 1500)
 }
 
 const computerScore = () => {
-  console.log(dice)
+  console.log(compDice)
   if (d1.innerHTML == d2.innerHTML && d2.innerHTML == d3.innerHTML) {
     compStatus.innerHTML = `Banker rolled triple ${d1.innerHTML}'s`
   } else if (d1.innerHTML == d2.innerHTML) {
     compStatus.innerHTML = `Banker rolled a ${d3.innerHTML}`
+    console.log(JSON.stringify(d3.innerHTML))
+    let index = heiarchy.indexOf(d3.innerHTML)
+    console.log(index)
   } else if (d1.innerHTML == d3.innerHTML) {
     compStatus.innerHTML = `Banker rolled a ${d2.innerHTML}`
+    console.log(JSON.stringify(d2.innerHTML))
+    let index = heiarchy.indexOf(d2.innerHTML)
+    console.log(index)
   } else if (d2.innerHTML == d3.innerHTML) {
     compStatus.innerHTML = `Banker rolled a ${d1.innerHTML}`
-  } else if (JSON.stringify(dice) === JSON.stringify(['1', '2', '3'])) {
-    console.log('automatic loss')
-  } else if (JSON.stringify(dice) === JSON.stringify(['4', '5', '6'])) {
-    console.log('headcrack')
+    console.log(JSON.stringify(d1.innerHTML))
+    let index = heiarchy.indexOf(d1.innerHTML)
+    console.log(index)
+  } else if (JSON.stringify(compDice) === JSON.stringify(['1', '2', '3'])) {
+    compStatus.innerHTML = `1-2-3 Banker Loses`
+  } else if (JSON.stringify(compDice) === JSON.stringify(['4', '5', '6'])) {
+    compStatus.innerHTML = `HEADCRACK BANKER WINS`
+  } else {
+    compStatus.innerHTML = `Banker Rolls Again`
+    computerRoll()
   }
 }
 
@@ -46,8 +80,46 @@ const playerRoll = () => {
   d4.innerHTML = Math.floor(Math.random() * 6 + 1)
   d5.innerHTML = Math.floor(Math.random() * 6 + 1)
   d6.innerHTML = Math.floor(Math.random() * 6 + 1)
+  playerDice = []
+  playerDice.push(d4.innerHTML, d5.innerHTML, d6.innerHTML)
+  playerDice = playerDice.sort()
+  playerScore()
+}
+
+const playerScore = () => {
+  console.log(playerDice)
+  if (d4.innerHTML == d5.innerHTML && d5.innerHTML == d6.innerHTML) {
+    playerStatus.innerHTML = `You rolled triple ${d4.innerHTML}'s`
+  } else if (d4.innerHTML == d5.innerHTML) {
+    playerStatus.innerHTML = `You rolled a ${d6.innerHTML}`
+    let www = heiarchy.indexOf(d6.innerHTML)
+    console.log(www)
+  } else if (d4.innerHTML == d6.innerHTML) {
+    playerStatus.innerHTML = `You rolled a ${d5.innerHTML}`
+    console.log(JSON.stringify(d5.innerHTML))
+    let www = heiarchy.indexOf(d5.innerHTML)
+    console.log(www)
+  } else if (d5.innerHTML == d6.innerHTML) {
+    playerStatus.innerHTML = `You rolled a ${d4.innerHTML}`
+    console.log(JSON.stringify(d4.innerHTML))
+    let www = heiarchy.indexOf(d4.innerHTML)
+    console.log(www)
+  } else if (JSON.stringify(playerDice) === JSON.stringify(['1', '2', '3'])) {
+    playerStatus.innerHTML = `1-2-3 You Lose`
+  } else if (JSON.stringify(playerDice) === JSON.stringify(['4', '5', '6'])) {
+    playerStatus.innerHTML = `HEADCRACK YOU WIN`
+  } else {
+    playerStatus.innerHTML = `Roll Again`
+  }
+}
+
+const checkForWinner = () => {
+  if (JSON.stringify(compDice) === JSON.stringify[('4', '5', '6')]) {
+    console.log('computer wins')
+  }
 }
 
 playBtn.addEventListener('click', computerRoll)
+// playBtn.style.visibility = 'none'
 
 rollDiceBtn.addEventListener('click', playerRoll)
