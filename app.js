efadd73ee8c595d093a8276c4f77d0511da3b661
Austin LePorin <austinleporin@gmail.com>
2022-07-06 10:46:35 -0400

    Detecting winner on double roll

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ let playerStatus = document.querySelector('.playerScore')
 let compDice = []
 let playerDice = []
 
+// const playerOne = banker
+// const playerTwo = user
+
 let d1 = document.querySelector('#d1')
 let d2 = document.querySelector('#d2')
 let d3 = document.querySelector('#d3')
@@ -18,6 +21,10 @@ let d5 = document.querySelector('#d5')
 let d6 = document.querySelector('#d6')
 
 let gameOver = false
+
+let playerIndex
+
+let compIndex
 
 let heiarchy = [
   '1-2-3',
@@ -54,18 +61,18 @@ const computerScore = () => {
   } else if (d1.innerHTML == d2.innerHTML) {
     compStatus.innerHTML = `Banker rolled a ${d3.innerHTML}`
     console.log(JSON.stringify(d3.innerHTML))
-    let index = heiarchy.indexOf(d3.innerHTML)
-    console.log(index)
+    compIndex = heiarchy.indexOf(d3.innerHTML)
+    // console.log(compIndex)
   } else if (d1.innerHTML == d3.innerHTML) {
     compStatus.innerHTML = `Banker rolled a ${d2.innerHTML}`
     console.log(JSON.stringify(d2.innerHTML))
-    let index = heiarchy.indexOf(d2.innerHTML)
-    console.log(index)
+    compIndex = heiarchy.indexOf(d2.innerHTML)
+    // console.log(compIndex)
   } else if (d2.innerHTML == d3.innerHTML) {
     compStatus.innerHTML = `Banker rolled a ${d1.innerHTML}`
     console.log(JSON.stringify(d1.innerHTML))
-    let index = heiarchy.indexOf(d1.innerHTML)
-    console.log(index)
+    compIndex = heiarchy.indexOf(d1.innerHTML)
+    // console.log(compIndex)
   } else if (JSON.stringify(compDice) === JSON.stringify(['1', '2', '3'])) {
     compStatus.innerHTML = `1-2-3 Banker Loses`
   } else if (JSON.stringify(compDice) === JSON.stringify(['4', '5', '6'])) {
@@ -92,18 +99,18 @@ const playerScore = () => {
     playerStatus.innerHTML = `You rolled triple ${d4.innerHTML}'s`
   } else if (d4.innerHTML == d5.innerHTML) {
     playerStatus.innerHTML = `You rolled a ${d6.innerHTML}`
-    let www = heiarchy.indexOf(d6.innerHTML)
-    console.log(www)
+    playerIndex = heiarchy.indexOf(d6.innerHTML)
+    // console.log(playerIndex)
   } else if (d4.innerHTML == d6.innerHTML) {
     playerStatus.innerHTML = `You rolled a ${d5.innerHTML}`
     console.log(JSON.stringify(d5.innerHTML))
-    let www = heiarchy.indexOf(d5.innerHTML)
-    console.log(www)
+    playerIndex = heiarchy.indexOf(d5.innerHTML)
+    // console.log(playerIndex)
   } else if (d5.innerHTML == d6.innerHTML) {
     playerStatus.innerHTML = `You rolled a ${d4.innerHTML}`
     console.log(JSON.stringify(d4.innerHTML))
-    let www = heiarchy.indexOf(d4.innerHTML)
-    console.log(www)
+    playerIndex = heiarchy.indexOf(d4.innerHTML)
+    // console.log(playerIndex)
   } else if (JSON.stringify(playerDice) === JSON.stringify(['1', '2', '3'])) {
     playerStatus.innerHTML = `1-2-3 You Lose`
   } else if (JSON.stringify(playerDice) === JSON.stringify(['4', '5', '6'])) {
@@ -111,15 +118,18 @@ const playerScore = () => {
   } else {
     playerStatus.innerHTML = `Roll Again`
   }
+  checkForWinner()
 }
 
 const checkForWinner = () => {
-  if (JSON.stringify(compDice) === JSON.stringify[('4', '5', '6')]) {
-    console.log('computer wins')
+  console.log(playerIndex, compIndex)
+  if (playerIndex > compIndex) {
+    console.log('winner')
+  } else if (playerIndex < compIndex) {
+    console.log('Loser')
   }
 }
 
 playBtn.addEventListener('click', computerRoll)
-// playBtn.style.visibility = 'none'
 
 rollDiceBtn.addEventListener('click', playerRoll)
